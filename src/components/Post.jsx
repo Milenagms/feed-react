98b307465c8ac3a083cdec095c837ce3c6849cf2
Milenagms <milenagomes022@gmail.com>
2022-09-21 18:11:22 -0300

    first components

---
+++ src/components/Post.jsx
@@ -0,0 +1,26 @@
+import styles from './Post.module.css'
+
+export function Post() {
+    return (
+        <article className={styles.post}>
+            <header>
+                <div className={styles.author}>
+                    <img className={styles.avatar} src={"https://images.unsplash.com/photo-1542740348-39501cd6e2b4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=300&q40"} alt={"user"}/>
+                    <div className={styles.authorInfo}>
+                        <strong>Erica Fernandes</strong>
+                        <span>Designer</span>
+                    </div>
+                </div>
+                <time  title={"20 de setembro de 2022 às 08:13h"} dateTime={"2022-09-20 12:00:00"}>Publicado há 1h</time>
+            </header>
+
+            <div className={styles.content}>
+                <p>Fala galeraa 👋</p>
+                <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+                <p>👉 <a href={""}>jane.design/doctorcare</a></p>
+                <p> <a href={""}>#novoprojeto #nlw #rocketseat</a></p>
+            </div>
+
+        </article>
+    )
+}
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,30 @@
+import { PencilLine } from 'phosphor-react'
+import styles from './sidebar.module.css'
+
+
+export function Sidebar() {
+    return (
+        <aside className={styles.sidebar}>
+            <img
+                className={styles.cover}
+                src="https://images.unsplash.com/photo-1617042375876-a13e36732a04?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=40" alt="background"
+            />
+            <div className={styles.profile}>
+                <img
+                    className={styles.avatar}
+                    src={"https://github.com/Milenagms.png"}
+                    alt={"avatar profile"}
+                />
+                <strong>Milena Gomes</strong>
+                <span> Web Developer</span>
+            </div>
+
+            <footer>
+                    <a href="#">
+                        <PencilLine size={20}/>
+                        Editar o seu perfil
+                    </a>
+            </footer>
+        </aside>
+    )
+}
