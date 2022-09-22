5453de896c1421149fe47425686751c589fa76f2
Milenagms <milenagomes022@gmail.com>
2022-09-22 15:36:17 -0300

    part of the post finished

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -21,6 +21,18 @@ export function Post() {
                 <p> <a href={""}>#novoprojeto #nlw #rocketseat</a></p>
             </div>
 
+            <form className={styles.commentForm}>
+                <strong> Deixe seu feedback</strong>
+
+                <textarea
+                    placeholder={"Deixe um comentário"}
+                />
+                <footer>
+                    <button type={"submit"}>Comentar</button>
+                </footer>
+
+            </form>
+
         </article>
     )
 }
